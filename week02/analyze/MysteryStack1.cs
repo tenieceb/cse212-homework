ed371bb18dc5ebb269a0f6e4c5fedd9db5f3508f
tenieceb <tabusenbark@gmail.com>
2025-01-18 17:29:01 -0700

    figured out how mysterystack 1 and 2 worked

--- week02/analyze/MysteryStack1.cs
+++ week02/analyze/MysteryStack1.cs
@@ -1,13 +1,20 @@
 ﻿public static class MysteryStack1 {
     public static string Run(string text) {
-        var stack = new Stack<char>();
-        foreach (var letter in text)
+        var stack = new Stack<char>();//a new stack where the letters of the text will be placed
+        foreach (var letter in text)//separates each letter of the word/text and pushes them to the stack
             stack.Push(letter);
 
         var result = "";
-        while (stack.Count > 0)
-            result += stack.Pop();
+        while (stack.Count > 0)//checks the stack count to be sure there's something in the stack as it goes through the stack, when the stack is empty it will end the while loop
+            result += stack.Pop();//adds a letter to the result starting at the end and removes them from the stack.
 
-        return result;
+        return result;//returns the string of the result.
     }
 }
+
+// Purpose of MysteryStack1.Run is to reverse the input text.
+
+// string text = "racecar"
+// result = "racecar"
+// stressed = "desserts"
+// a nut for a jar of tuna = "anut fo raj a rof tun a"
--- week02/analyze/MysteryStack2.cs
+++ week02/analyze/MysteryStack2.cs
@@ -1,49 +1,55 @@
 ﻿public static class MysteryStack2 {
     private static bool IsFloat(string text) {
-        return float.TryParse(text, out _);
+        return float.TryParse(text, out _);//checks to see if the text can be turned into a float, returns true or false depending on if TryParse worked
     }
 
     public static float Run(string text) {
         var stack = new Stack<float>();
-        foreach (var item in text.Split(' ')) {
-            if (item == "+" || item == "-" || item == "*" || item == "/") {
-                if (stack.Count < 2)
+        foreach (var item in text.Split(' ')) {//iterates through the text looking only at the items and ignoring any spaces.
+            if (item == "+" || item == "-" || item == "*" || item == "/") {//if the item is +, -, *, or / this will run
+                if (stack.Count < 2) //if the stack count less than 2 it will throw an exception "invalid case 1"
                     throw new ApplicationException("Invalid Case 1!");
 
-                var op2 = stack.Pop();
-                var op1 = stack.Pop();
+                var op2 = stack.Pop();// removes the most recently added item  and assigns it to op2 
+                var op1 = stack.Pop();// removes the second most recently additem and assigns it to op1
                 float res;
-                if (item == "+") {
+                if (item == "+") {//if the item is a "+" this will add the op1 and op2 values together
                     res = op1 + op2;
                 }
-                else if (item == "-") {
+                else if (item == "-") { // if the item is "-" it will subtract op2 from op1
                     res = op1 - op2;
                 }
-                else if (item == "*") {
+                else if (item == "*") { // if the item is "*" it will multiply the two values
                     res = op1 * op2;
                 }
-                else {
-                    if (op2 == 0)
+                else {//If the value is none of the above this will start
+                    if (op2 == 0)// if op2 = 0 then the "Invalid Case 2" error will throw
                         throw new ApplicationException("Invalid Case 2!");
 
-                    res = op1 / op2;
+                    res = op1 / op2;// if op2 is not 0 then this will op1,op2
                 }
 
-                stack.Push(res);
+                stack.Push(res); //Adds whatever the res is to the end of our stack.
             }
-            else if (IsFloat(item)) {
-                stack.Push(float.Parse(item));
+            else if (IsFloat(item)) {//Uses IsFloat to see if the item can be a float number
+                stack.Push(float.Parse(item));//adds the item to the end of the stack after changing it to a float number
             }
-            else if (item == "") {
+            else if (item == "") {//if the item is nothing, nothing  happens
             }
-            else {
+            else {// any other instance throws an exception "invalid Case 3"
                 throw new ApplicationException("Invalid Case 3!");
             }
         }
 
-        if (stack.Count != 1)
+        if (stack.Count != 1)//checks the stack count and if it is not equal to 1 "invalid Case 4" exception is thrown
             throw new ApplicationException("Invalid Case 4!");
 
-        return stack.Pop();
+        return stack.Pop();//removes and returns the number of the stack
     }
 }
+
+// program uses the most recent addition to the stack to determine if it is retaining the number for the stack or using the numbers to do the appropriate arithmatic
+// string text = "5 3 7 + *"
+// result= 50
+// string text = "6 2 + 5 3 - / " 
+//result = 4
