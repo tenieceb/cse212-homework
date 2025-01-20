b7aea3d5160bbd29d2de2752c350e39aa401e4a5
tenieceb <tabusenbark@gmail.com>
2025-01-20 15:37:06 -0700

    finished week2

--- week02/code/PriorityQueue_Tests.cs
+++ week02/code/PriorityQueue_Tests.cs
@@ -6,24 +6,78 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 public class PriorityQueueTests
 {
     [TestMethod]
-    // Scenario: 
-    // Expected Result: 
-    // Defect(s) Found: 
+    // Scenario: Enqueue separate items at various priorities.
+    // Expected Result: Regardless of the priority, the item is enqueued to the end of the line.
+    // Defect(s) Found: None. Enqueued as expected.
     public void TestPriorityQueue_1()
     {
         var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
+        priorityQueue.Enqueue("Item 1",3);
+        priorityQueue.Enqueue("Item 2",4);
+        priorityQueue.Enqueue("Item 3", 1);
+        Console.WriteLine(priorityQueue);
+        priorityQueue.Enqueue("Item 4", 5);
+        Console.WriteLine(priorityQueue);
+
     }
 
     [TestMethod]
-    // Scenario: 
-    // Expected Result: 
-    // Defect(s) Found: 
+    // Scenario: The item with the highest priority is in the center of the queue.
+    // Expected Result: The system will dequeue the item regardless of it's spot within the system.
+    // Defect(s) Found: The item that is "dequeued" is not the item that is the highest priority due to line 27 in the main code _queue.Count -1 leaves the end of the list off.
     public void TestPriorityQueue_2()
     {
         var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
+        priorityQueue.Enqueue("Item 1", 3);
+        priorityQueue.Enqueue("Item 2", 4);
+        priorityQueue.Enqueue("Item 3", 1);
+        priorityQueue.Enqueue("Item 4", 5);
+        var dequeuedByPriority = priorityQueue.Dequeue();
+        var highestPriority = "Item 4";
+        if (highestPriority != dequeuedByPriority)
+        {
+            Assert.Fail($"Expected: {highestPriority} but got {dequeuedByPriority}.");
+        }
     }
 
-    // Add more test cases as needed below.
+    [TestMethod]
+        // Scenario: The item with the highest priority is in the center of the queue.
+        // Expected Result: The system will dequeue the item regardless of it's spot within the system.
+        // Defect(s) Found: The system did not remove the item from the list. Added "_queue.RemoveAt(highPriorityIndex)"
+
+    public void TestPriorityQueue_3()
+    {
+
+
+        var priorityQueue = new PriorityQueue();
+        priorityQueue.Enqueue("Item 1",3);
+        priorityQueue.Enqueue("Item 2",4);
+        priorityQueue.Enqueue("Item 3", 1);
+        priorityQueue.Enqueue("Item 4", 4);
+        Console.WriteLine(priorityQueue);
+        var dequeuedByPriorityAndPosition = priorityQueue.Dequeue();
+        Console.WriteLine(dequeuedByPriorityAndPosition);
+        Console.WriteLine(priorityQueue);
+
+    }
+
+    [TestMethod]
+        // Scenario: There is no one in the queue
+        // Expected Result: System will throw an error exception
+        // Defect(s) Found: none
+
+    public void TestPriorityQueue_4()
+    {
+        var priorityQueue = new PriorityQueue();
+        try
+        {
+            priorityQueue.Dequeue(); // This should throw an exception
+            Assert.Fail("Expected exception was not thrown.");
+        }
+        catch (InvalidOperationException)
+        {
+            // Test passes if exception is caught
+        }
+    }
+
 }
--- week02/code/TakingTurnsQueue_Tests.cs
+++ week02/code/TakingTurnsQueue_Tests.cs
@@ -11,7 +11,7 @@ public class TakingTurnsQueueTests
     // Scenario: Create a queue with the following people and turns: Bob (2), Tim (5), Sue (3) and
     // run until the queue is empty
     // Expected Result: Bob, Tim, Sue, Bob, Tim, Sue, Tim, Sue, Tim, Tim
-    // Defect(s) Found: 
+    // Defect(s) Found: Queue was not removing people correctly
     public void TestTakingTurnsQueue_FiniteRepetition()
     {
         var bob = new Person("Bob", 2);
@@ -43,7 +43,7 @@ public class TakingTurnsQueueTests
     // Scenario: Create a queue with the following people and turns: Bob (2), Tim (5), Sue (3)
     // After running 5 times, add George with 3 turns.  Run until the queue is empty.
     // Expected Result: Bob, Tim, Sue, Bob, Tim, Sue, Tim, George, Sue, Tim, George, Tim, George
-    // Defect(s) Found: 
+    // Defect(s) Found: none
     public void TestTakingTurnsQueue_AddPlayerMidway()
     {
         var bob = new Person("Bob", 2);
